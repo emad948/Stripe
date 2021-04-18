908416197e2b1e6a7473c14609873b797f5440a4
Emad <almsouti.e@gmail.com>
2021-04-18 05:33:41 +0200

    Updates

---
+++ precache-manifest.f4aba5071c79f9021dd9cd544f529dde.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0db4f418753a1d0d9538f20b5a6bdab",
+    "url": "/Stripe/index.html"
+  },
+  {
+    "revision": "eff0b3a6e99e310b1c80",
+    "url": "/Stripe/static/css/main.758289ee.chunk.css"
+  },
+  {
+    "revision": "5c74610ae15f648ccfa9",
+    "url": "/Stripe/static/js/2.0415f4aa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Stripe/static/js/2.0415f4aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff0b3a6e99e310b1c80",
+    "url": "/Stripe/static/js/main.31f1cb66.chunk.js"
+  },
+  {
+    "revision": "8df455e174d9c00403bb",
+    "url": "/Stripe/static/js/runtime-main.bf3d9ebe.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/Stripe/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/Stripe/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/Stripe/static/media/phone.e908706c.svg"
+  }
+]);
