e812b12e684705161036ba80611f28d723d56aed
Emad <almsouti.e@gmail.com>
2021-04-18 05:36:07 +0200

    Typo

---
+++ src/Sidebar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import sublinks from './data'
+import { useGlobalContext } from './context';
+
+const Sidebar=() => {
+  const { closeSidebar, isSidebarOpen }=useGlobalContext();
+
+  return <aside className={`sidebar-wrapper ${isSidebarOpen&&"show"}`}>
+    <div className="sidebar">
+      <button className="close-btn" onClick={closeSidebar}>
+        <FaTimes />
+      </button>
+      <div className="sidebar-links">
+        {sublinks.map((item, index) => {
+          const { page, links }=item;
+          return (
+            <article key={index}>
+              <h4>{page}</h4>
+              <div className="sidebar-sublinks">
+                {links.map((link, index) => {
+                  const { label, icon, url }=link
+                  return (
+                    <a key={index} href={url} > {icon}
+                      {label}</a>
+
+                  )
+                })}
+              </div>
+            </article>
+          );
+        })}
+      </div>
+
+    </div>
+  </aside>
+}
+
+export default Sidebar
